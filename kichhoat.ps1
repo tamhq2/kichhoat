0759f0a3c1313706e244802e846f76dbfde2e77c
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2023-07-08 11:59:14 +0700

    Add files via upload

---
+++ kichhoat.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = "Stop"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/tamhq2/kichhoat/main/kichhoat.cmd'
+
+$rand = Get-Random -Maximum 1000
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\kichhoat_$rand.cmd" } else { "$env:TEMP\kichhoat_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\kichhoat*.cmd", "$env:SystemRoot\Temp\kichhoat*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
